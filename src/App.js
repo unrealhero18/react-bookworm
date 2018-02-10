5c140207207ed4103877a4f3cfef1e930bef07da
Filimonov Kostya <slayer64@yandex.ru>
2018-02-10 18:29:14 +0200

    User signs up

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { Route } from 'react-router-dom';
 import HomePage from './components/pages/HomePage';
 import LoginPage from './components/pages/LoginPage';
+import SignupPage from './components/pages/SignupPage';
 import DashboardPage from './components/pages/DashboardPage';
 import UserRoute from './components/routes/UserRoute';
 import GuestRoute from './components/routes/GuestRoute';
@@ -10,6 +11,7 @@ import GuestRoute from './components/routes/GuestRoute';
 const App = ({ location }) => <div className="ui container">
   <Route location={location} path="/" exact component={HomePage} />
   <GuestRoute location={location} path="/login" exact component={LoginPage} />
+  <GuestRoute location={location} path="/signup" exact component={SignupPage} />
   <UserRoute location={location} path="/dashboard" exact component={DashboardPage} />
 </div>;
 
